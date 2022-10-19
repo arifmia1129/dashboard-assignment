a4e76180b3c1c435f6e4e1c6a7ca4d34974ae32b
arifvtti <arif.vtti@gmail.com>
2022-10-19 19:05:36 +0600

    initial commit

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LeftSide from './LeftSide';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <LeftSide />
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/Pages/LeftSide.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LeftSide = () => {
+    return (
+        <div className='bg-[#0F3460] text-white'>
+            <div>
+                <p className='font-bold'>Dashboard</p>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LeftSide;
